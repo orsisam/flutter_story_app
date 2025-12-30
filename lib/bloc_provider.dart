a94f7f331777763fea6dacbf5945144258746ae7
Isro Hidayatulloh <orsi.amax@gmail.com>
2025-12-30 17:51:18 +0700

    Create Provider List

---
+++ lib/bloc_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_story_app/data/datasources/auth_remote_datasource.dart';
+import 'package:flutter_story_app/data/datasources/story_remote_datasource.dart';
+import 'package:flutter_story_app/presentation/auth/blocs/login/login_bloc.dart';
+import 'package:flutter_story_app/presentation/auth/blocs/logout/logout_bloc.dart';
+import 'package:flutter_story_app/presentation/auth/blocs/register/register_bloc.dart';
+import 'package:flutter_story_app/presentation/profile/blocs/get_profile/get_profile_bloc.dart';
+import 'package:flutter_story_app/presentation/story/blocs/create_story/create_story_bloc.dart';
+import 'package:flutter_story_app/presentation/story/blocs/delete_story/delete_story_bloc.dart';
+import 'package:flutter_story_app/presentation/story/blocs/get_stories/get_stories_bloc.dart';
+import 'package:flutter_story_app/presentation/story/blocs/update_story/update_story_bloc.dart';
+
+// =========================
+// DATASOURCE (singleton)
+// =========================
+final AuthRemoteDatasource _authDatasource = AuthRemoteDatasource();
+final StoryRemoteDatasource _storyDatasource = StoryRemoteDatasource();
+
+// =========================
+// BLOC PROVIDERS
+// =========================
+List<BlocProvider> get blocProviders => [
+  // Auth BLoCs
+  BlocProvider<LoginBloc>(create: (_) => LoginBloc(_authDatasource)),
+  BlocProvider(create: (_) => RegisterBloc(_authDatasource)),
+  BlocProvider(create: (_) => LogoutBloc(_authDatasource)),
+
+  // Profile BLoC
+  BlocProvider(create: (_) => GetProfileBloc(_authDatasource)),
+
+  // Story BLoCs
+  BlocProvider(create: (_) => GetStoriesBloc(_storyDatasource)),
+  BlocProvider(create: (_) => CreateStoryBloc(_storyDatasource)),
+  BlocProvider(create: (_) => UpdateStoryBloc(_storyDatasource)),
+  BlocProvider(create: (_) => DeleteStoryBloc(_storyDatasource)),
+];
+
+// ================================
+// HELPER FUNCTION
+// ================================
+AuthRemoteDatasource get authDatasource => _authDatasource;
+StoryRemoteDatasource get storyDatasource => _storyDatasource;
