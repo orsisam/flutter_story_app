58e0698c439d0232f2ac36cf5d9dc9b45b864349
Isro Hidayatulloh <orsi.amax@gmail.com>
2026-01-04 18:41:08 +0700

    Create story pages

---
+++ lib/presentation/story/pages/edit_story_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditStoryPage extends StatefulWidget {
+  const EditStoryPage({super.key});
+
+  @override
+  State<EditStoryPage> createState() => _EditStoryPageState();
+}
+
+class _EditStoryPageState extends State<EditStoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/story/widgets/story_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class StoryCard extends StatelessWidget {
+  const StoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
